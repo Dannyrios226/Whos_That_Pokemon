eddbbe891a01381c31c25e39db46ebbbdbbd315d
Dannyrios226 <dannyrios226@gmail.com>
2025-07-09 11:24:09 -0700

    add base to vite config file

--- src/App.jsx
+++ src/App.jsx
@@ -15,60 +15,62 @@ function App() {
   const [pokemonWrongId2, setPokemonNameWrong2] = useState(0);
   const [pokemonWrongId3, setPokemonNameWrong3] = useState(0);
 
+  const [options, setOptions] = useState([])
+  const [Score, setScore] = useState(0);
+  let number = 0;
+
   useEffect(() => {
     getRandomPokemon();
     // fetchPokemon(pokemonId);
   }, []);
 
   async function fetchPokemon(num) {
-    const fetchResponse = await fetch("https://pokeapi.co/api/v2/pokemon/" + num);
-    const result = await fetchResponse.json()
+    const fetchResponse = await fetch(
+      "https://pokeapi.co/api/v2/pokemon/" + num
+    );
+    const result = await fetchResponse.json();
 
     console.log(result);
     let pokeInfo = {
       url: result.sprites.front_default,
-      name: result.forms[0].name
+      name: result.forms[0].name,
     };
-    
-return pokeInfo
-
+    console.log(pokeInfo);
+    return pokeInfo;
   }
 
-function shuffleArray(array) {
+  function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
-}
-
+  }
 
   async function getRandomPokemon() {
-    const randomId = Math.floor(Math.random() * highRange) + lowRange;
-    const randomIdWrong1 = Math.floor(Math.random() * highRange) + lowRange;
-    const randomIdWrong2 = Math.floor(Math.random() * highRange) + lowRange;
-    const randomIdWrong3 = Math.floor(Math.random() * highRange) + lowRange;
+    const randomId =
+      Math.floor(Math.random() * (highRange - lowRange + 1)) + lowRange;
+    const randomIdWrong1 =
+      Math.floor(Math.random(highRange - lowRange + 1)) + lowRange;
+    const randomIdWrong2 =
+      Math.floor(Math.random() * (highRange - lowRange + 1)) + lowRange;
+    const randomIdWrong3 =
+      Math.floor(Math.random() * (highRange - lowRange + 1)) + lowRange;
     setPokemonId(randomId);
     const correctPokeInfo = await fetchPokemon(randomId);
-    setPokemonUrl(correctPokeInfo.url)
-    setPokemonName(correctPokeInfo.name)
-    console.log("correct", correctPokeInfo)
-    
+    setPokemonUrl(correctPokeInfo.url);
+    setPokemonName(correctPokeInfo.name);
+    console.log("correct", correctPokeInfo);
 
-        const wrongPokemonInfo = await fetchPokemon(randomIdWrong1);
-        const wrongPokemonInfo2 = await fetchPokemon(randomIdWrong2);
-        const wrongPokemonInfo3 = await fetchPokemon(randomIdWrong3);
+    const wrongPokemonInfo = await fetchPokemon(randomIdWrong1);
+    const wrongPokemonInfo2 = await fetchPokemon(randomIdWrong2);
+    const wrongPokemonInfo3 = await fetchPokemon(randomIdWrong3);
     // fetchPokemon(randomIdWrong3);
 
     setPokemonNameWrong1(wrongPokemonInfo.name);
     setPokemonNameWrong2(wrongPokemonInfo2.name);
     setPokemonNameWrong3(wrongPokemonInfo3.name);
-
-
   }
-
-
-  function ShowPokemonName() {}
 
   console.log(pokemonName);
 
@@ -80,9 +82,10 @@ function shuffleArray(array) {
         display: "flex",
         border: "25px solid",
         color: "grey.800",
-        borderColor: "grey.300",
+        borderColor: "#333",
         display: "flex",
         flexDirection: "column",
+        background: "linear-gradient(to bottom, #FFCDD2, #E57373)",
         height: "auto",
         width: "auto",
       }}
@@ -91,7 +94,9 @@ function shuffleArray(array) {
       <Container maxWidth="sm">
         <Typography
           sx={{
+            display: "flex",
             fontSize: "30px",
+            justifyContent: "center",
           }}
         >
           WHO'S THAT POKEMON
@@ -105,20 +110,61 @@ function shuffleArray(array) {
           flexDirection: "row",
           justifyContent: "center",
           alignItems: "flex-start",
-          color: "grey.800",
+          color: "#D32F2F",
           "& > *": {
             border: "3px solid",
-            color: "grey.800",
-            borderColor: "grey.300",
+            color: "#333",
+            borderColor: "#333",
+            backgroundColor: "##DCDCDC ",
             width: "15vw",
+            fontWeight: "bold",
           },
         }}
       >
         {/* item3A */}
-        <Box maxWidth="sm">Gen1</Box>
-        <Box maxWidth="sm">Gen2</Box>
-        <Box maxWidth="sm">Gen3</Box>
-        <Box maxWidth="sm">Gen4</Box>
+        <button
+          onClick={() => {
+            setLowRange(1);
+            setHighRange(151);
+            alert("NOW SHOWING ONLY FIRST GENERATION POKEMON");
+            getRandomPokemon();
+          }}
+        >
+          <Box maxWidth="sm"> FIRST GEN</Box>
+        </button>
+
+        <button
+          onClick={() => {
+            setLowRange(152);
+            setHighRange(251);
+            alert("NOW SHOWING ONLY SECOND GENERATION POKEMON");
+            getRandomPokemon();
+          }}
+        >
+          <Box maxWidth="sm">SECOND GEN</Box>
+        </button>
+
+        <button
+          onClick={() => {
+            setLowRange(252);
+            setHighRange(386);
+            alert("NOW SHOWING ONLY THIRD GENERATION POKEMON");
+            getRandomPokemon();
+          }}
+        >
+          <Box maxWidth="sm">THIRD GEN</Box>
+        </button>
+
+        <button
+          onClick={() => {
+            setLowRange(387);
+            setHighRange(493);
+            alert("NOW SHOWING ONLY FOURTH GENERATION POKEMON");
+            getRandomPokemon();
+          }}
+        >
+          <Box maxWidth="sm">FOURTH GEN</Box>
+        </button>
       </Box>
 
       {/* box4 */}
@@ -129,50 +175,82 @@ function shuffleArray(array) {
           display: "flex",
           flexDirection: "row",
           mx: "auto",
+          padding: 2,
           "& > *": {
             border: "4px solid",
-            color: "grey.800",
-            borderColor: "grey.300",
+            borderColor: "#333",
+            backgroundColor: "#FAF3E0",
+            borderColor: "##000000 ",
             width: "15vw",
-            height: "65vh",
+            height: "60vh",
+            backgroundColor: "#FFFF",
+            padding: 2,
           },
         }}
       >
-        {/* item4A */}
         <Box
           sx={{
             alignItems: "center",
             overflow: "hidden",
           }}
         >
-          Box 1
           <img src={pokemonUrl} style={{ width: "100%", height: "auto" }} />
         </Box>
-
-        {/* item4B */}
         <Box
           sx={{
             gap: 1,
             display: "flex",
             flexDirection: "column",
+            alignItems: "center",
             mx: "auto",
             "& > *": {
               border: "4px solid",
-              color: "grey.800",
-              borderColor: "grey.300",
+              color: "#333",
+              borderColor: "##000000 ",
+              background: "#E0E0E0",
+              fontWeight: "bold",
               width: "12vw",
               height: "14vh",
             },
           }}
         >
-          Box 2{/* container4Ba */}
-          { shuffleArray([
-            <Container maxWidth="sm">{pokemonName}</Container>,
-            <Container maxWidth="sm">{pokemonWrongId1}</Container>,
-            <Container maxWidth="sm">{pokemonWrongId2}</Container>,
-            <Container maxWidth="sm">{pokemonWrongId3}</Container>,
-          ]) }
-          
+          {shuffleArray([
+            <button
+              onClick={() => {
+                getRandomPokemon();
+                setScore((prev) => prev + 5);
+              }}
+            >
+              <Container maxWidth="sm">{pokemonName}</Container>
+            </button>,
+
+            <button
+              onClick={() => {
+                getRandomPokemon();
+                alert("WRONG ANSWER");
+              }}
+            >
+              <Container maxWidth="sm">{pokemonWrongId1}</Container>
+            </button>,
+
+            <button
+              onClick={() => {
+                getRandomPokemon();
+                alert("WRONG ANSWER");
+              }}
+            >
+              <Container maxWidth="sm">{pokemonWrongId2}</Container>
+            </button>,
+
+            <button
+              onClick={() => {
+                getRandomPokemon();
+                alert("WRONG ANSWER");
+              }}
+            >
+              <Container maxWidth="sm">{pokemonWrongId3}</Container>
+            </button>,
+          ])}
         </Box>
 
         {/* item 3 */}
@@ -180,37 +258,47 @@ function shuffleArray(array) {
           sx={{
             gap: 5,
             display: "flex",
-            justifyContent: "-moz-initial",
             flexDirection: "column",
+            alignItems: "center",
             mx: "auto",
             "& > *": {
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
               border: "4px solid",
-              color: "grey.800",
-              borderColor: "grey.300",
+              color: "#333",
+              borderColor: "##000000 ",
+              background: "#E0E0E0",
+              fontWeight: "bold",
               width: "12vw",
               height: "14vh",
             },
           }}
         >
-          Box 3{/* container4Ca */}
-          <Container maxWidth="sm"></Container>
-          {/* container4Cb */}
-          <Container maxWidth="sm"></Container>
+          <Container maxWidth="sm">SCORE: {Score}</Container>
+          <button
+            onClick={() => {
+              setScore(0);
+            }}
+          >
+            <Container maxWidth="sm">RESET SCORE</Container>
+          </button>
+
           {/* container 4Cc */}
-          <Container maxWidth="sm"></Container>
+          <button
+            onClick={() => {
+              setLowRange(1);
+              setHighRange(1025);
+              alert("NOW SHOWING ALL POKEMON");
+              getRandomPokemon();
+            }}
+          >
+            <Container maxWidth="sm"> SHOW ALL POKEMON</Container>
+          </button>
         </Box>
       </Box>
     </Container>
   );
 }
 export default App;
-
-// <TextField
-//   id="outlined-controlled"
-//   label="Name"
-//   value={""}
-//   onChange={(event) => {
-//     // empty for now
-//   }}
-
-// />
